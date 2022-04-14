3ee8b297413e9a4b982aadbdaeef3a586a795742
jinojoseph <jinojoseph@chevron.com>
2022-04-14 17:09:51 +0530

    complete the event notification application

--- event-notifications/event-notification-webui/src/App.js
+++ event-notifications/event-notification-webui/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import SearchBar from "./Components/SearchBar/Search";
+import GetData from "./Components/GetData/GetData";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <Header />
         <SearchBar />
+        <GetData />
       </header>
     </div>
   );
--- event-notifications/event-notification-webui/src/Components/GetData/GetData.js
+++ event-notifications/event-notification-webui/src/Components/GetData/GetData.js
@@ -1,2 +1,57 @@
 import React from "react";
+import "./GetData.css";
+class GetData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
+  componentWillMount() {
+    setInterval(() => {
+      fetch("http://localhost:4000/events/")
+        .then((res) => res.json())
+        .then((json) => {
+          this.setState({
+            items: json,
+          });
+        });
+    }, 500);
+  }
+  render() {
+    const { items } = this.state;
+    return (
+      <div className="events">
+        <table className="event_details">
+          <tr>
+            <th>UUID</th>
+            <th>CREATED_TIME</th>
+            <th>DETAILS</th>
+            
+          </tr>
+          {items.map((item) => {
+            return (
+              <tr key={item.uuid}>
+              
+                <td>{item.uuid}</td>
+                <td>{item.createdAt}</td>
+                {/* <td>{JSON.stringify(item.event_details.POD)}</td>
+                <td>{JSON.stringify(item.event_details.POD_IP)}</td>
+                <td>{JSON.stringify(item.event_details.STATUS)}</td>
+                <td>{JSON.stringify(item.event_details.NAMESPACE)}</td> */}
+                {/* <td>
+                  {JSON.stringify(item.event_details.RUNNING_POD_HOSTNAME)}
+                </td> */}
 
+                <td>{JSON.stringify(item.event_details)}</td>
+                
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    );
+  }
+}
+
+export default GetData;
